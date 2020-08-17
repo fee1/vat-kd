7464f1e8af95eabc876427df82022933e4a6c896
zxf <1063446979@qq.com>
2020-08-17 08:35:13 +0800

    注解使用说明

--- src/main/java/com/bw/customannotations/ReadDatasource.java
+++ src/main/java/com/bw/customannotations/ReadDatasource.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 /**
  * @author zhuxf
  * 读写分离的第一种方式的注解类
- * 通过类或者方法上的注解区分使用那个数据源，主要使用在mapper接口类
+ * 通过类或者方法上的注解区分使用那个数据源，注意不要在mapper接口类使用，主从数据库的同步存在延迟，在mapper上可能会导致数据的不一致
  */
 @Target({ElementType.METHOD,ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
